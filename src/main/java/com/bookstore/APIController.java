a6eeb39a4286fc866c30d0fcec3522503132c068
hlt0408 <huanglangtao08@gmail.com>
2016-05-13 15:37:12 -0400

    controller and restcontroller split to two java files

--- src/main/java/com/bookstore/APIController.java
+++ src/main/java/com/bookstore/APIController.java
@@ -1,21 +1,23 @@
 package com.bookstore;
 
 import java.util.concurrent.atomic.AtomicLong;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
-public class GreetingController {
+@RestController
+public class APIController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	@RequestMapping("/greetings")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
